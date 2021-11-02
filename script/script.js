bd988e66c557b2171148a9d7871858a4f6b77ff3
sanchesleo <sanchesleoalves@gmail.com>
2021-11-02 13:00:37 -0300

    Adicionando efeito de scroll suave e melhoria do menu mobile

--- script/script.js
+++ script/script.js
@@ -1,8 +1,28 @@
+import ScrollSuave from "./modules/scroll_suave.js";
+
 const btnMobile = document.querySelector('.check-btn');
+let openMenu = false;
+
+const iconMobile = document.querySelector('.fas');
 
 function handleRenderMenu() {
+    openMenu = !openMenu;
+    console.log(openMenu);
     const navMenu = document.querySelector('.lista-menu');
     navMenu.classList.toggle('active');
+
+    console.log(iconMobile);
+
+    if (openMenu) {
+        iconMobile.classList.remove('fa-bars');
+        iconMobile.classList.add('fa-times');
+    }else{
+        iconMobile.classList.add('fa-bars');
+        iconMobile.classList.remove('fa-times');
+    }
 }
 
 btnMobile.addEventListener('click',handleRenderMenu);
+
+const scrollSuave = new ScrollSuave('a[href^="#"]');
+scrollSuave.init();
